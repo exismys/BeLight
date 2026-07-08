556ea05facdef516c8d8cadc978ad73cdfa282ee
Ritesh Patel <riteshzeronity@gmail.com>
2026-07-08 11:04:06 +0530

    Implement flat shading - computer light intensity per triangle

--- include/rasterizer.hpp
+++ include/rasterizer.hpp
@@ -25,6 +25,7 @@ struct Triangle3D {
     Vec3 p1;
     Vec3 p2;
     Color color;
+    Vec3 normal;
 };
 
 struct Plane{
@@ -50,16 +51,33 @@ struct Object {
     Vec3 position;
 };
 
+enum class LightTypeRast {
+    Point,
+    Directional,
+    Ambient
+};
+struct LightRast {
+    LightTypeRast type;
+    float intensity;
+    Vec3 position;
+    Vec3 direction;
+};
+
 struct Scene_Rast {
     std::vector<std::unique_ptr<Mesh>> meshes;
     std::vector<Object> objects;
+    std::vector<LightRast> light_sources;
     Camera camera;
 
     ObjectMode object_mode;
 };
 
 Scene_Rast create_scene_rast();
+
 bool is_back_face(Triangle3D& triangle);
+Vec3 get_triangle_normal(Triangle3D& triangle);
+float compute_lighting_rast(Vec3 normal, Vec3 point, Scene_Rast& scene); 
+
 std::vector<Object> clip_scene(std::vector<Object>& objects, Plane planes[]);
 Object clip_object(Object& object, Plane planes[]);
 Object clip_object_against_plane(Object object, Plane plane);
@@ -67,17 +85,19 @@ std::vector<Triangle3D> clip_triangle(Triangle3D triangle, std::span<Plane> plan
 std::vector<Triangle3D> clip_triangle_against_plane(Triangle3D& triangle, Plane& plane);
 Vec3 plane_line_intersection(Vec3 a, Vec3 b, Plane plane);
 float signed_distance(Vec3 vertex, Plane& plane);
+
+
 void render_scene_rast(Renderer& renderer, Scene_Rast& scene);
 Mesh create_cube_mesh();
-void render_object(Renderer& renderer, Object& object, Mat4& view);
-void render_triangle(Renderer& renderer, const Triangle3D& triangle);
+void render_object(Renderer& renderer, Object& object, Mat4& view, Scene_Rast& scene);
+void render_triangle(Renderer& renderer, const Triangle3D& triangle, Scene_Rast& scene);
 
 std::vector<float> interpolate(Vec2 p1, Vec2 p2);
 
 void draw_line(Renderer& renderer, Vec2 p1, Vec2 p2, Color color);
 
 void draw_triangle_wireframe(Renderer& renderer, Vec2 p1, Vec2 p2, Vec2 p3, Color color);
-void draw_triangle_filled(Renderer& renderer, const Triangle3D& triangle);
+void draw_triangle_filled(Renderer& renderer, const Triangle3D& triangle, Scene_Rast& scene);
 void draw_triangle_shaded(Renderer& renderer, Vec2 p1, Vec2 p2, Vec2 p3, Color color, float h1, float h2, float h3);
 
 Vec2 viewport_to_canvas(Renderer& renderer, Vec3 point);
